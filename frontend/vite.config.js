d7a919a0ce3871943d90efd85953d9d51eb9a667
mageshwarigowri <mageshwarigowri@gmail.com>
2026-07-03 22:43:35 +0530

    updating

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hopehands-website/', // 👈 ADD THIS LINE (use your exact repository name)
 })
